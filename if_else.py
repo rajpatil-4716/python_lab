3a6f38b17450ea34b95ab608b9b0743c1718e397
rajpatil-4716 <rajpatil4716@gmail.com>
2026-02-27 10:18:23 +0530

    26-02-2026

---
+++ if_else.py
@@ -0,0 +1,5 @@
+a=float(input("Enter A : "))
+if a>0:
+    print("A Is Positive")
+else:
+    print("A Is Negative")
---
+++ odd-even.py
@@ -0,0 +1,7 @@
+a=int(input("Enter A : "))
+b=int(input("Enter B : "))
+
+if a%2==0:
+    print("A Is Even")
+else:
+    print("B Is Odd")
