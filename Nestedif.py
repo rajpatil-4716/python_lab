2b369eb72bb4d5bba9ca9080de784dc1a6ff5342
rajpatil-4716 <rajpatil4716@gmail.com>
2026-02-27 10:05:14 +0530

    26-02-2026

---
+++ Nestedif.py
@@ -0,0 +1,15 @@
+a=int(input("Enter A : "))
+b=int(input("Enter B : "))
+c=int(input("Enter C : "))
+
+if a>b:
+    if a>c:
+        print("A Is Max")
+    else:
+        print("C Is Max")
+elif b>c:
+    print("B Is Max")
+else:
+    print("C Is Max")
+        
+        
